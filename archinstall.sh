cecc8d31d7aa0f09c291b0faf257387dee84c1da
Tahseenx <tahseenkamal.pro@gmail.com>
2024-11-17 18:23:31 +0000

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -6,27 +6,27 @@ paru
 # Install packages
 paru -S --needed base-devel bluez bluez-utils brave-bin brightnessctl fastfetch foot gnome-themes-extra grim gvfs hyprland hyprpolkitagent-git hyprshade ibus-avro-git lib32-nvidia-utils lib32-opencl-nvidia libreoffice-fresh mpv ntfs-3g nwg-look p7zip papirus-icon-theme slurp swayimg thunar thunar-archive-plugin thunar-volman tumbler wl-clipboard wofi xarchiver
 
-cd
-
 # Bashrc and Vimrc
 cp .bashrc .vimrc /home/monon/
 
 # Copy Hyprland config
-cp hyprland.conf /home/monon/.config/hypr/
+cp hyprland.conf ~/.config/hypr/
 
 # Fastfetch config
-mkdir /home/monon/.config/fastfetch
-cp config.jsonc /home/monon/.config/fastfetch/
+mkdir -p ~/.config/fastfetch
+cp config.jsonc ~/.config/fastfetch/
 
 # Foot config
-mkdir /home/monon/.config/foot
-cp foot.ini /home/monon/.config/foot/
+mkdir -p ~/.config/foot
+cp foot.ini ~/.config/foot/
 
 # Swayimg config
-mkdir /home/monon/.config/swayimg
-cp config /home/monon/.config/swayimg/
+mkdir -p ~/.config/swayimg
+cp config ~/.config/swayimg/
 
 echo "  
      v  v  v  v  v  v  v  v  v  v  v  v  
   => Installation of Packages complete! <=
-     ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^"
+     ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
+     
+     "
