304f4e82f94c630a2ac0f9716e6edeba9d1eda13
Tahseenx <tahseenkamal.pro@gmail.com>
2024-11-17 10:27:11 -0500

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -4,6 +4,6 @@
 paru
 
 # Install packages
-paru -S --needed base-devel bluez bluez-utils brave-bin brightnessctl fastfetch foot gnome-themes-extra grim gvfs hyprland hyprpolkitagent-git hyprshade ibus-avro-git lib32-nvidia-utils lib32-opencl-nvidia libreoffice-fresh mpv ntfs-3g nwg-look p7zip papirus-icon-theme slurp steam swayimg thunar thunar-archive-plugin thunar-volman tumbler wl-clipboard wofi xarchiver
+paru -Sy --needed base-devel bluez bluez-utils brave-bin brightnessctl fastfetch foot gnome-themes-extra grim gvfs hyprland hyprpolkitagent-git hyprshade ibus-avro-git lib32-nvidia-utils lib32-opencl-nvidia libreoffice-fresh mpv ntfs-3g nwg-look p7zip papirus-icon-theme slurp steam swayimg thunar thunar-archive-plugin thunar-volman tumbler wl-clipboard wofi xarchiver
 
 echo ":: Installation of Packages complete"
