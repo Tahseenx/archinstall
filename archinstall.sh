837868e4abd2a0d99729ce94fec96856f05f8d0e
Tahseenx <tahseenkamal.pro@gmail.com>
2024-11-17 11:18:10 -0500

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -4,7 +4,7 @@
 paru
 
 # Install packages
-paru -S --needed --noconfirm base-devel bluez bluez-utils brave-bin brightnessctl fastfetch foot gnome-themes-extra grim gvfs hyprland hyprpolkitagent-git hyprshade ibus-avro-git lib32-nvidia-utils lib32-opencl-nvidia libreoffice-fresh mpv ntfs-3g nwg-look p7zip papirus-icon-theme slurp steam swayimg thunar thunar-archive-plugin thunar-volman tumbler wl-clipboard wofi xarchiver
+paru -S --needed --noconfirm base-devel bluez bluez-utils brave-bin brightnessctl fastfetch foot gnome-themes-extra grim gvfs hyprland hyprpolkitagent-git hyprshade ibus-avro-git lib32-nvidia-utils lib32-opencl-nvidia libreoffice-fresh mpv ntfs-3g nwg-look p7zip papirus-icon-theme slurp swayimg thunar thunar-archive-plugin thunar-volman tumbler wl-clipboard wofi xarchiver
 
 # Bashrc and Vimrc
 cp .bashrc .vimrc /home/monon/
