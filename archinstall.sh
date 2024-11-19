7394bb5195c49c103b89f8855622720585727038
Tahseenx <tahseenkamal.pro@gmail.com>
2024-11-19 14:38:18 -0500

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-# Update package lists
-paru
+# Update pacman.conf
+sudo cp pacman.conf /etc/
+
+# Paru install
+cd..
+sudo git clone https://aur.archlinux.org/paru.git
+sudo chown $USER:$USER paru/
+cd paru/
+makepkg -si
+cd ..
+
+# Add CachyOS repository
+curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
+tar xvf cachyos-repo.tar.xz && cd cachyos-repo
+sudo ./cachyos-repo.sh
+cd ..
+cd archinstall/
 
 # Install packages
-paru -S --needed base-devel bluez bluez-utils brave-bin brightnessctl cpupower dosfstools fastfetch foot gnome-themes-extra grim gvfs hyprland hyprpolkitagent-git hyprshade ibus-avro-git lib32-nvidia-utils lib32-opencl-nvidia libreoffice-fresh mkinitcpio-firmware mpv ntfs-3g nwg-look p7zip papirus-icon-theme slurp swayimg thunar thunar-archive-plugin thunar-volman tlp tumbler wl-clipboard wofi xarchiver
+paru -S --needed base-devel brave-bin breeze-gtk brightnessctl cpupower dosfstools fastfetch foot grim gvfs hyprland hyprpolkitagent-git hyprshade libreoffice-fresh linux-cachyos-bore linux-cachyos-bore-headers mkinitcpio-firmware mpv ntfs-3g nwg-look p7zip slurp swayimg thunar thunar-archive-plugin thunar-volman tlp tumbler vim wl-clipboard wofi xarchiver
 
 # Bashrc and Vimrc
 cp .bashrc .vimrc /home/monon/
@@ -25,9 +40,13 @@ cp foot.ini ~/.config/foot/
 mkdir -p ~/.config/swayimg
 cp config ~/.config/swayimg/
 
+# Add BL Hyprshade shader
+sudo cp bl.glsl /usr/share/hyprshade/shaders/
+
 echo "  
+     v
      v  v  v  v  v  v  v  v  v  v  v  v  
   => Installation of Packages complete! <=
      ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
-     
+     ^
      "
