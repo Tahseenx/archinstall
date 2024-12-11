749fa5bf030d888f1ced242cd187be73c9ae44a5
Taseen Kamal <tahseenkamal.pro@gmail.com>
2024-12-11 00:32:37 -0500

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -19,7 +19,7 @@ cd ..
 cd archinstall/
 
 # Install packages
-paru -S --needed base-devel brave-bin breeze-gtk brightnessctl cpupower dosfstools fastfetch foot grim gvfs hyprland hyprpolkitagent-git hyprshade libreoffice-fresh linux-cachyos-bore linux-cachyos-bore-headers mkinitcpio-firmware mpv ntfs-3g nwg-look p7zip slurp swayimg thunar thunar-archive-plugin thunar-volman tlp tumbler vim wl-clipboard wofi xarchiver
+paru -S --needed base-devel brave-bin breeze-gtk brightnessctl cpupower dosfstools fastfetch foot grim gvfs hyprland hyprpolkitagent-git hyprshade libreoffice-fresh linux-cachyos-bore linux-cachyos-bore-headers mkinitcpio-firmware mpv nwg-look p7zip slurp swayimg thunar thunar-archive-plugin thunar-volman tlp tumbler vim wl-clipboard wofi xarchiver
 
 # Bashrc and Vimrc
 cp .bashrc .vimrc /home/monon/
@@ -46,11 +46,12 @@ sudo cp cpupower /etc/default/
 # Add Hyprshade blue light filter shader
 sudo cp bl.glsl /usr/share/hyprshade/shaders/
 
-# Add msfonts
+# Add Microsoft default fonts
 cp -r .fonts /home/monon/
 
 # Enable necessary services
 sudo systemctl enable tlp
 sudo systemctl enable cpupower
 
+# Reboot the machine
 reboot
