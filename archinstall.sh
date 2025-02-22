c72052b92323c5dd54070023fe760f1e4cb3c6c3
Taseen Kamal <tahseenkamal.pro@gmail.com>
2025-02-21 23:08:07 -0500

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -4,54 +4,48 @@
 # sudo cp pacman.conf /etc/
 
 # Paru install
-cd ..
+cd /opt/
 sudo git clone https://aur.archlinux.org/paru.git
 sudo chown $USER:$USER paru/
 cd paru/
 makepkg -si
-cd ..
 
 # Add CachyOS repository
+mkdir -p ~/Downloads
+cd ~/Downloads
+
 curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz && cd cachyos-repo
 sudo ./cachyos-repo.sh
-cd ..
-cd archinstall/
+cd /opt/archinstall/
 
 # Install packages
-paru -S --needed base-devel brave-bin breeze-gtk brightnessctl fastfetch foot grim gvfs htop hyprland hyprsunset libreoffice-fresh linux-cachyos-bore linux-cachyos-bore-headers mkinitcpio-firmware mpv nwg-look p7zip qbittorrent reflector slurp steam swayimg thunar thunar-archive-plugin tumbler vim wl-clipboard wofi xarchiver
+paru -S --needed base-devel brave-bin breeze-gtk brightnessctl fastfetch foot grim gvfs htop hyprland hyprsunset libreoffice-fresh mkinitcpio-firmware mpv nwg-look qbittorrent slurp swayimg thunar thunar-archive-plugin tumbler vim wl-clipboard wofi xarchiver
 
 # Bashrc and Vimrc
 cp .bashrc .vimrc ~/
 
 # Copy Hyprland config
-mkdir -p ~/.config/hypr
+mkdir ~/.config/hypr
 cp hyprland.conf ~/.config/hypr/
-cp hyprshade.toml ~/.config/hypr/
 
 # Fastfetch config
-mkdir -p ~/.config/fastfetch
+mkdir ~/.config/fastfetch
 cp config.jsonc ~/.config/fastfetch/
 
 # Foot config
-mkdir -p ~/.config/foot
+mkdir ~/.config/foot
 cp foot.ini ~/.config/foot/
 
 # Swayimg config
-mkdir -p ~/.config/swayimg
+mkdir ~/.config/swayimg
 cp config ~/.config/swayimg/
 
 # Cpupower config
 # sudo cp cpupower /etc/default/
 
-# Add Hyprshade blue light filter shader
-# sudo cp /usr/share/hyprshade/shaders/blue-light-filter.glsl /usr/share/hyprshade/shaders/bl.glsl
-
-# Auto Hyprshade on/off
-# sudo systemctl --user enable --now hyprshade.timer
-
 # Add Microsoft default fonts
-cp -r .fonts /home/monon/
+cp -r .fonts ~/
 
 # Enable necessary services
 # sudo systemctl enable tlp
