f6093a5ef88bba1e6f3e8a61b41e1cfb784ee945
Taseen Kamal <tahseenkamal.pro@gmail.com>
2025-10-04 22:40:50 +0600

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -10,6 +10,10 @@ cd
 # Install Packages
 paru -S --needed 7zip brightnessctl dosfstools fastfetch foot git hyprland hyprshot hyprsunset linux-cachyos-bore linux-cachyos-bore-headers linux-cachyos-bore-nvidia-open mkinitcpio-firmware ungoogled-chromium-bin vim wofi
 
+# Install Wine
+paru -S wine-ge-custom
+paru -S --needed --asdeps giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite libxcomposite lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader sdl2 lib32-sdl2
+
 cd ~/archinstall
 
 # Bashrc and Vimrc
