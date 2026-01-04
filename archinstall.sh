058bd3e5e4698925104dce6e328ee3593af93287
Taseen Kamal <tahseenkamal.pro@gmail.com>
2026-01-04 14:57:42 +0600

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # Add CachyOS Repos
-cd
-curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
-tar xvf cachyos-repo.tar.xz && cd cachyos-repo
-sudo ./cachyos-repo.sh
-cd
+#cd
+#curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
+#tar xvf cachyos-repo.tar.xz && cd cachyos-repo
+#sudo ./cachyos-repo.sh
+#cd
 
 # Install Packages
-paru -S --needed 7zip brightnessctl dosfstools fastfetch foot git hyprland hyprshot hyprsunset linux-cachyos-bore linux-cachyos-bore-headers linux-cachyos-bore-nvidia-open mkinitcpio-firmware firefox thunar thunar-archive-plugin gvfs tumbler mpv vim xarchiver wofi
+paru -S --needed brightnessctl fastfetch foot hyprland hyprsunset mpv vim wofi
 
-# Install Wine
-paru -S wine-ge-custom
-paru -S --needed --asdeps giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite libxcomposite lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader sdl2 lib32-sdl2
+# Wine dependencies
+#paru -S --needed --asdeps giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite libxcomposite lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader sdl2 lib32-sdl2
 
 cd ~/archinstall
 
@@ -29,7 +28,9 @@ cp foot.ini ~/.config/foot/
 
 # GTK settings.ini
 mkdir -p ~/.config/gtk-3.0
-cp settings.ini ~/.config/gtk-3.0
+mkdir -p ~/.config/gtk-4.0
+cp settings.ini ~/.config/gtk-3.0/
+cp settings.ini ~/.config/gtk-4.0/
 
 # Add Microsoft default fonts
 cp -r .fonts ~/
